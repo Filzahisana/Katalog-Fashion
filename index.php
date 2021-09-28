6b96f993cd18e7e3d31f02979591dbf25f1504ea
unknown <filzahisana@gmail.com>
2021-09-29 02:52:40 +0700

    update

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include 'lib/navbar.php';
+include 'lib/view/landing.php'
+//$conn = mysqli_connect("localhost", "root", "", "katalog fashion");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div id="nav">
+
+    </div>
+</body>
+
+</html>
